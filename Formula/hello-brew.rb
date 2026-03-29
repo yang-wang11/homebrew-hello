2ce5b840db2143b9f63bbe838f7eef66f557a960
elan <yang.wang11@sap.com>
2026-03-29 23:44:06 +0800

    fix the url

--- Formula/hello-brew.rb
+++ Formula/hello-brew.rb
@@ -8,14 +8,12 @@ class HelloBrew < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/yang-wang11/homebrew-hello/releases/download/v0.3.0/hello-brew-aarch64-apple-darwin.tar.gz
-"
-      sha256 "207c8cbb9523b5a1b5874418e8775f2e198fd265af8a937da12799e35fcccb29"
+      url "https://github.com/yang-wang11/homebrew-hello/releases/download/v0.3.0/hello-brew-aarch64-apple-darwin.tar.gz"
+      sha256 "c8ed6c0e00544b8c7fe75eb75dff4b4853ec090bdd4ee2fd4df90848f41e32f2"
     end
     on_intel do
-      url "https://github.com/yang-wang11/homebrew-hello/releases/download/v0.3.0/hello-brew-x86_64-apple-darwin.tar.gz
-"
-      sha256 "08d1c54af9fcfa6610f95f3271d716570f1279491b65bcdfcbd81c3e41bb9352"
+      url "https://github.com/yang-wang11/homebrew-hello/releases/download/v0.3.0/hello-brew-x86_64-apple-darwin.tar.gz"
+      sha256 "4c41320e61e97f81b4781de0cc11df1738544572bae9d758ac6af40ea780e18a"
     end
   end
 
